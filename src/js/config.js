73e9495dc38e5d71607c0506385b63e0d8a4b18d
srujankumar126 <ramisettysrujankumar@gmail.com>
2021-07-31 22:15:16 +0530

    final commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a781d6e1-0fbe-47e4-b586-c2eb1c8c7ddc';
+export const MODAL_CLOSE_SEC = 2.5;
